c0756ce25db72fce0ff647fa3ce330896c1de2c2
Lars Dormans <lars.dormans@live.nl>
2023-12-03 05:20:27 +0100

    Initial commit

---
+++ Plugin.cs
@@ -0,0 +1,15 @@
+﻿using BepInEx;
+
+namespace LethalShock
+{
+    [BepInPlugin(PluginInfo.PLUGIN_GUID, PluginInfo.PLUGIN_NAME, PluginInfo.PLUGIN_VERSION)]
+    public class LethalShock : BaseUnityPlugin
+    {
+        private readonly Harmony harmony = new Harmony("LethalShock");
+        private void Awake()
+        {
+            // Plugin startup logic
+            Logger.LogInfo($"Plugin {PluginInfo.PLUGIN_GUID} is loaded!");
+        }
+    }
+}
