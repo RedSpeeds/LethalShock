e6d8c5e4c6411fb2b7bef4a15b0d638836b6c838
Lars Dormans <lars.dormans@live.nl>
2023-12-04 00:06:47 +0100

    Things work now however its not pretty since i cant seem to use Costura for some reason

--- Plugin.cs
+++ Plugin.cs
@@ -26,12 +26,12 @@ namespace LethalShock
         internal readonly string pishockLogId = "LethalShock (Lethal company)";
         private readonly Random rnd = new();
 
-        internal static LethalShock instance;
+        internal static LethalShock instance = null;
 
         internal ManualLogSource mls;
         internal ConfigEntry<string> pishockUsername;
         internal ConfigEntry<string> pishockApiKey;
-        internal ConfigEntry<List<string>> pishockCodes;
+        internal ConfigEntry<string> pishockCodes;
 
         internal ConfigEntry<bool> shockOnDeath;
         internal ConfigEntry<bool> shockOnDamage;
@@ -54,56 +54,55 @@ namespace LethalShock
             // Plugin startup logic
             mls = BepInEx.Logging.Logger.CreateLogSource("LethalShock");
             mls.LogInfo("LethalShock initiated");
-            pishockUsername = this.Config.Bind<string>("LethalShock", "username", "");
-            pishockApiKey = this.Config.Bind<string>("LethalShock", "apikey", "");
-            pishockCodes = this.Config.Bind<List<string>>("LethalShock", "codes", ["code1", "code2"]);
-            shockOnDeath = this.Config.Bind<bool>("LethalShock", "shockOnDeath", true, "Enable to get shocked when you die");
-            shockOnDamage = this.Config.Bind<bool>("LethalShock", "shockOnDamage", true, "Enable to get shocked when you take damage (Based on the damage value)");
-            vibrateOnly = this.Config.Bind<bool>("LethalShock", "vibrateOnly", false, "Enable to only use vibrations");
-            warningVibration = this.Config.Bind<bool>("LethalShock", "warnning", true, "Enable to get a warning vibration before a shock");
-            shockBasedOnHealth = this.Config.Bind<bool>("LethalShock", "shockByHealth", false, "Enable to calculate shock intensity based on your remaining health instead of the damage taken (shockOnDeath must be enabled)");
-            duration = this.Config.Bind<int>("LethalShock", "duration", 1, "Duration of the shock/vibration");
-            maxIntensity = this.Config.Bind<int>("LethalShock", "max", 80, new ConfigDescription("Maximum intensity of the shock/vibration", new AcceptableValueRange<int>(1, 100)));
-            minIntensity = this.Config.Bind<int>("LethalShock", "min", 1, new ConfigDescription("Minimum intensity of the shock/vibration", new AcceptableValueRange<int>(1, 100)));
-            shockMode = this.Config.Bind<ShockModes>("LethalShock", "mode", ShockModes.RANDOM, "What to do when you have multiple shockers?");
-            durationDeath = this.Config.Bind<int>("LethalShock", "durationDeath", 5, "Duration of the shock/vibration when you die");
-            intensityDeath = this.Config.Bind<int>("LethalShock", "intensityDeath", 100, "The intensity of the shock/vibration when you die");
-            modeDeath = this.Config.Bind<ShockModes>("LethalShock", "modeDeath", ShockModes.ALL, "What to do when you have multiple shockers when you die");
+            pishockUsername = Config.Bind("LethalShock", "username", "");
+            pishockApiKey = Config.Bind("LethalShock", "apikey", "");
+            pishockCodes = Config.Bind("LethalShock", "codes", "code1,code2");
+            shockOnDeath = Config.Bind("LethalShock", "shockOnDeath", true, "Enable to get shocked when you die");
+            shockOnDamage = Config.Bind("LethalShock", "shockOnDamage", true, "Enable to get shocked when you take damage (Based on the damage value)");
+            vibrateOnly = Config.Bind("LethalShock", "vibrateOnly", false, "Enable to only use vibrations");
+            warningVibration = Config.Bind("LethalShock", "warnning", true, "Enable to get a warning vibration before a shock");
+            shockBasedOnHealth = Config.Bind("LethalShock", "shockByHealth", false, "Enable to calculate shock intensity based on your remaining health instead of the damage taken (shockOnDeath must be enabled)");
+            duration = Config.Bind("LethalShock", "duration", 1, "Duration of the shock/vibration");
+            maxIntensity = Config.Bind("LethalShock", "max", 80, new ConfigDescription("Maximum intensity of the shock/vibration", new AcceptableValueRange<int>(1, 100)));
+            minIntensity = Config.Bind("LethalShock", "min", 1, new ConfigDescription("Minimum intensity of the shock/vibration", new AcceptableValueRange<int>(1, 100)));
+            shockMode = Config.Bind("LethalShock", "mode", ShockModes.RANDOM, "What to do when you have multiple shockers?");
+            durationDeath = Config.Bind("LethalShock", "durationDeath", 5, "Duration of the shock/vibration when you die");
+            intensityDeath = Config.Bind("LethalShock", "intensityDeath", 100, "The intensity of the shock/vibration when you die");
+            modeDeath = Config.Bind("LethalShock", "modeDeath", ShockModes.ALL, "What to do when you have multiple shockers when you die");
             harmony.PatchAll(typeof(LethalShock));
-            harmony.PatchAll(typeof(PlayerControllerB_DamagePlayer));
-            harmony.PatchAll(typeof(PlayerControllerB_KillPlayer));
+            harmony.PatchAll(typeof(PlayerControllerBPatch));
             if (instance == null)
             {
                 instance = this;
             }
         }
 
-        internal void doDamage(int dmg, int health)
+        internal void DoDamage(int dmg, int health)
         {
             int maxDmgShock = Mathf.Clamp(dmg, minIntensity.Value, maxIntensity.Value);
             int shockHealth = 100 - health;
             int maxHealthShock = Mathf.Clamp(shockHealth, minIntensity.Value, maxIntensity.Value);
             if (shockBasedOnHealth.Value)
             {
-                mls.LogDebug("Shocking based on health for " + maxHealthShock);
+                mls.LogInfo("Shocking based on health for " + maxHealthShock);
                 DoOperation(maxHealthShock, duration.Value, shockMode.Value);
             }
             else
             {
-                mls.LogDebug("Shocking based on health for " + maxDmgShock);
+                mls.LogInfo("Shocking based on health for " + maxDmgShock);
                 DoOperation(maxDmgShock, duration.Value, shockMode.Value);
             }
         }
-        internal void doDeath()
+        internal void DoDeath()
         {
-            mls.LogDebug("Death shock");
+            Logger.LogInfo("Death shock");
             DoOperation(intensityDeath.Value, durationDeath.Value, modeDeath.Value);
         }
 
         private void DoOperation(int intensity, int duration, ShockModes mode)
         {
-            string[] codes = pishockCodes.Value.ToArray();
-            bool[] picked = pickShockers(mode, codes.Length);
+            string[] codes = pishockCodes.Value.Split(',');
+            bool[] picked = PickShockers(mode, codes.Length);
             for (int i = 0; i < codes.Length; i++)
             {
                 mls.LogDebug("Running DoOperation for shocker coded " + codes[i]);
@@ -120,7 +119,11 @@ namespace LethalShock
                     pishock.SendVibrationAsync(user, intensity, duration);
                     if (!vibrateOnly.Value)
                     {
-                        Task.Delay(duration*1000).ContinueWith(t => { pishock.SendShockAsync(user, intensity, duration); }).Start();
+                        Task.Run(async () =>
+                        {
+                            await Task.Delay(duration * 1000);
+                            _ = pishock.SendShockAsync(user, intensity, duration);
+                        });
                     }
                 }
                 else
@@ -138,7 +141,7 @@ namespace LethalShock
 
         private int roundRobin = 0;
 
-        private bool[] pickShockers(ShockModes mode, int length)
+        private bool[] PickShockers(ShockModes mode, int length)
         {
             bool[] shocks = new bool[length];
             int ranindex = rnd.Next(0, length);
@@ -188,20 +191,19 @@ namespace LethalShock
 }
 namespace LethalShock.Patches
 {
-    [HarmonyPatch(typeof(PlayerControllerB), nameof(PlayerControllerB.DamagePlayer))]
-    internal class PlayerControllerB_DamagePlayer
+    internal class PlayerControllerBPatch
     {
-        private static void PostFix(int ___health, int damageNumber)
+        [HarmonyPatch(typeof(PlayerControllerB), nameof(PlayerControllerB.KillPlayer))]
+        [HarmonyPostfix]
+        private static void DeathPatch()
         {
-            LethalShock.instance.doDamage(damageNumber, ___health);
+            LethalShock.instance.DoDeath();
         }
-    }
-    [HarmonyPatch(typeof(PlayerControllerB), nameof(PlayerControllerB.KillPlayer))]
-    internal class PlayerControllerB_KillPlayer
-    {
-        private static void Postfix()
+        [HarmonyPatch(typeof(PlayerControllerB), nameof(PlayerControllerB.DamagePlayer))]
+        [HarmonyPostfix]
+        private static void DamagePatch(int ___health, int damageNumber)
         {
-            LethalShock.instance.doDeath();
+            LethalShock.instance.DoDamage(damageNumber, ___health);
         }
     }
 
